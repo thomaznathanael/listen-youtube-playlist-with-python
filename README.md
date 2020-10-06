a99703323762e18a68a5d452bc2c38b007a83df4
Emanoel S. Vieira <dev.emanoel@gmail.com>
2020-10-05 21:21:58 -0300

    Atualizaçao para linux

--- README.md
+++ README.md
@@ -3,28 +3,16 @@ This is an example of how to listen music from a youtube playlist.
 
 ## Installation
 
-Install the all dependencies
+Install the all dependencies:
+```bash
+pip install -r requirements.txt
+```
+ou:
+```bash
+pip3 install -r requirements.txt
+```
 
-The dependency of pafy:
-```bash
-pip install youtube_dl
-```
-The pafy module:
-```bash
-pip install pafy
-```
-The vlc module:
-```bash
-pip install python-vlc
-```
-The keyboard module for keys detection:
-```bash
-pip install keyboard
-```
-The colored module:
-```bash
-pip install colored
-```
+intale o aplicativo VLC no seu sistema também
 
 ## Usage
 
@@ -40,7 +28,7 @@ playlist = pafy.get_playlist(url) # Extracts the playlist information
 ...
 
 ```
-For the next song press the "Page Up" key on your keyboard.
+For the next song press the "Page Up" key on your keyboard ou em caso de falha da biblioteca você terá que apertar enter no terminal.(geralmente acontece no linux)
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- youtube-playlist.py
+++ youtube-playlist.py
@@ -7,7 +7,7 @@ cyan = fg('cyan')
 red = fg('red')
 res = attr('reset')
 
-url = "PUT_YOUR_YOUTUBE_PLAYLIST_URL_HERE"
+url = "https://www.youtube.com/watch?v=gUjeFa5kS1Q&list=PLNuNMMvxVXyN87bkk_qi-FlI770zKGkOd"
 playlist = pafy.get_playlist(url) # Extracts playlist information
 
 for music in playlist['items']:
@@ -26,9 +26,16 @@ for music in playlist['items']:
     player.set_media(Media)
     player.play()
     sleep(3)
-    while player.is_playing():
-        key = keyboard.read_key() # Detects the pressed key
-        if key == 'page up':
-            print(f'{cyan}-next{res}')
-            break
-    player.stop()
+    try:
+        while player.is_playing():
+            key = keyboard.read_key() # Detects the pressed key
+            if key == 'page up':
+                print(f'{cyan}-next{res}')
+                break
+        player.stop()        
+    except:
+        print('Esse sistema não aceita comandos do teclado ainda')
+        input('Precione enter aki para ir a proxima música\n')
+
+
+#colored-1.4.2 keyboard-0.13.5 pafy-0.5.5 python-vlc-3.0.11115 youtube-dl-2020.9.20
