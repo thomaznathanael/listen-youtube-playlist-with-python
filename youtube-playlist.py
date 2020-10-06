c52a0c6fe02eb12824fc87b44f2c22088e608314
Thomaz Nathanael <51930484+thomaznathanael@users.noreply.github.com>
2020-10-05 22:04:50 -0300

    Update youtube-playlist.py

--- youtube-playlist.py
+++ youtube-playlist.py
@@ -2,21 +2,46 @@ import pafy
 import vlc
 from time import sleep
 import keyboard
-from colored import attr, fg
+from colored import attr, fg, bg
 cyan = fg('cyan')
 red = fg('red')
 res = attr('reset')
+black = fg('black')
+bg_y = bg('yellow')
 
-url = "PUT_YOUR_YOUTUBE_PLAYLIST_URL_HERE"
-playlist = pafy.get_playlist(url) # Extracts playlist information
+def playlist_(playlist):
+    print(f'{bg_y}{black} Playlist {res}')
+    for music in playlist['items']:
+        url_video = music['playlist_meta']['encrypted_id'] # Extracts the url from the videos
+        video = pafy.new(url_video)
+        audio = video.getbestaudio() # Extracts only the audio from the videos
+        playurl = audio.url
 
-for music in playlist['items']:
-    url_video = "https://www.youtube.com/watch?v=" + music['playlist_meta']['encrypted_id'] # Extrai o link de cada vídeo da playlist
+        print(music['playlist_meta']['title'] + f' - {red}' + music['playlist_meta']['duration'] + f'{res}')
+
+        # Below is the standard media reproduction of the python-vlc library
+        Instance = vlc.Instance()
+        player = Instance.media_player_new()
+        Media = Instance.media_new(playurl)
+        Media.get_mrl()
+        player.set_media(Media)
+        player.play()
+        sleep(3)
+        while player.is_playing():
+            key = keyboard.read_key() # Detects the pressed key
+            if key == 'page up':
+                print(f'{cyan}-next{res}')
+                break
+        player.stop()
+
+
+def only_one(url_video):
     video = pafy.new(url_video)
     audio = video.getbestaudio() # Extracts only the audio from the videos
     playurl = audio.url
+    print(f'{bg_y}{black} Single {res}')
 
-    print(music['playlist_meta']['title'] + f' - {red}' + music['playlist_meta']['duration'] + f'{res}')
+    #print(music['playlist_meta']['title'] + f' - {red}' + music['playlist_meta']['duration'] + f'{res}')
 
     # Below is the standard media reproduction of the python-vlc library
     Instance = vlc.Instance()
@@ -27,8 +52,20 @@ for music in playlist['items']:
     player.play()
     sleep(3)
     while player.is_playing():
-        key = keyboard.read_key() # Detects the pressed key
-        if key == 'page up':
-            print(f'{cyan}-next{res}')
-            break
+        pass
     player.stop()
+
+url = "https://www.youtube.com/watch?v=8SbUC-UaAxE&list=PLIS6Lred47C5-DpMjKxZI0UqodeZc4bKS"
+
+if 'list' in url:
+    try:
+        playlist_(pafy.get_playlist(url)) # Extracts playlist information and call the playlist function
+    except:
+        print('The url is not right')
+else:
+    try:
+        only_one(url)  # Call the one song function
+    except:
+        print('The url is not right')
+
+    
